e066341d67f302c0e82ff33f4eda11fbc66865b2
Anderson Lopes <andersonlopes017.al@gmail.com>
2024-02-28 10:49:41 -0400

    fix: correções pontuais

--- src/hooks/auth.jsx
+++ src/hooks/auth.jsx
@@ -8,28 +8,29 @@ function AuthProvider({ children }) {
   const [data, setData] = useState({});
 
   async function signIn({ email, password }) {
-
     try {
-      const response = await api.post('/sessions', { email, password });
+      const response = await api.post("/sessions", { email, password });
       const { user, token } = response.data;
 
-      api.defaults.headers.authorization = `Bearer ${token}`;
-      setData(user, token);
+      api.defaults.headers.Authorization = `Bearer ${token}`;
+      setData({ user, token });
 
     } catch (error) {
       if (error.response) {
         alert(error.response.data.message);
       } else {
-        alert("Não foi possivel entrar");
+        alert("Não foi possível acessar.");
       }
     }
   }
+
   return (
     <AuthContext.Provider value={{ signIn, user: data.user }}>
       {children}
     </AuthContext.Provider>
   )
 }
+
 function useAuth() {
   const context = useContext(AuthContext);
 
--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,16 +1,19 @@
-import { useState } from 'react';
-import { FiLock, FiMail } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
+import { useState } from "react";
 
-import { useAuth } from '../../hooks/auth';
+import { FiMail, FiLock } from "react-icons/fi";
+import { Link } from "react-router-dom";
 
-import { Button } from '../../components/Button';
-import { Input } from '../../components/Input';
-import { Background, Container, Form } from "./styels";
+import { useAuth } from "../../hooks/auth";
+
+import { Input } from "../../components/Input";
+import { Button } from "../../components/Button";
+
+import { Container, Form, Background } from "./styles";
+
 
 export function SignIn() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const { signIn } = useAuth();
 
@@ -27,22 +30,21 @@ export function SignIn() {
         <h2>Faça seu login</h2>
 
         <Input
+          type="text"
           placeholder="E-mail"
-          type='text'
           icon={FiMail}
           onChange={e => setEmail(e.target.value)}
         />
         <Input
+          type="password"
           placeholder="Senha"
-          type='password'
           icon={FiLock}
           onChange={e => setPassword(e.target.value)}
         />
 
-        <Button title="Entar" onClick={handleSignIn} />
+        <Button title="Entrar" onClick={handleSignIn} />
 
-        <Link to='/register'>Criar Conta</Link>
-
+        <Link to="/register">Criar conta</Link>
       </Form>
 
       <Background />
