d95a710c696b65b9d4a8f1d0782c1c1c277ca942
Anderson Lopes <andersonlopes017.al@gmail.com>
2024-02-28 09:15:52 -0400

    feat: implementando as funcionalidades do signIn

--- src/pages/SignIn/index.jsx
+++ src/pages/SignIn/index.jsx
@@ -1,11 +1,23 @@
+import { useState } from 'react';
 import { FiLock, FiMail } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
+
+import { useAuth } from '../../hooks/auth';
 
 import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
 import { Background, Container, Form } from "./styels";
 
 export function SignIn() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const { signIn } = useAuth();
+
+  function handleSignIn() {
+    signIn({ email, password });
+  }
+
   return (
     <Container>
       <Form>
@@ -18,14 +30,16 @@ export function SignIn() {
           placeholder="E-mail"
           type='text'
           icon={FiMail}
+          onChange={e => setEmail(e.target.value)}
         />
         <Input
           placeholder="Senha"
           type='password'
           icon={FiLock}
+          onChange={e => setPassword(e.target.value)}
         />
 
-        <Button title="Entar" />
+        <Button title="Entar" onClick={handleSignIn} />
 
         <Link to='/register'>Criar Conta</Link>
 
